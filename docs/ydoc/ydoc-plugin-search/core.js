6af12db57e32d3cb64bdf11d77b0956547022c0d
shuoshubao <759979885@qq.com>
2022-11-23 22:29:27 +0800

    docs: nothing

--- docs/ydoc/ydoc-plugin-search/core.js
+++ docs/ydoc/ydoc-plugin-search/core.js
@@ -1,27 +1,28 @@
-$(function() {
+$(function () {
   var num = 0;
   var searchText;
   var searchMaxNum = 8;
-  var releativePath = document.getElementById('releativePath').getAttribute('content') || ''
-  releativePath = releativePath.trim()
-  releativePath = releativePath === '' ? '.' : releativePath
+  var releativePath =
+    document.getElementById("releativePath").getAttribute("content") || "";
+  releativePath = releativePath.trim();
+  releativePath = releativePath === "" ? "." : releativePath;
 
-  window.ydoc_plugin_search_core = function(text) {
-    if(!text)return;
-    var json = cloneObject(window.ydoc_plugin_search_json);    
+  window.ydoc_plugin_search_core = function (text) {
+    if (!text) return;
+    var json = cloneObject(window.ydoc_plugin_search_json);
     searchText = text;
     num = 0;
-    var result = search(json)
+    var result = search(json);
     return result;
   };
 
   function cloneObject(obj) {
-    if (typeof obj === 'object') {
+    if (typeof obj === "object") {
       if (Array.isArray(obj)) {
         var newArr = [];
-        obj.forEach(function(item, index){
-          newArr[index] = cloneObject(item)
-        })
+        obj.forEach(function (item, index) {
+          newArr[index] = cloneObject(item);
+        });
         return newArr;
       } else {
         var newObj = {};
@@ -35,70 +36,74 @@ $(function() {
     }
   }
 
-
-  function search(json){
-    var result = {}
-    for(var i in json){
-      var data = searchBook(json[i])
+  function search(json) {
+    var result = {};
+    for (var i in json) {
+      var data = searchBook(json[i]);
       data = filterBook(data);
-      if(data.length > 0) result[i] = data;
+      if (data.length > 0) result[i] = data;
     }
     return result;
   }
 
-  function filterBook(newPages){
-    return newPages.filter(function(page){
-      if(!page.content && page.children.length === 0){
+  function filterBook(newPages) {
+    return newPages.filter(function (page) {
+      if (!page.content && page.children.length === 0) {
         return false;
       }
       return true;
-    })
+    });
   }
 
-  function searchBook(pages){
+  function searchBook(pages) {
     var newPages = [];
-    if(num > searchMaxNum){
+    if (num > searchMaxNum) {
       return newPages;
     }
-    for(var i=0, l=pages.length; i< l; i++){
-      var page = pages[i], searchPage = null;
-      if(num > searchMaxNum){
+    for (var i = 0, l = pages.length; i < l; i++) {
+      var page = pages[i],
+        searchPage = null;
+      if (num > searchMaxNum) {
         return newPages;
       }
-      if(page.content && page.content.toLowerCase().indexOf(searchText.toLowerCase()) !== -1){
+      if (
+        page.content &&
+        page.content.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+      ) {
         num++;
         searchPage = {
           title: page.title,
           content: page.content,
           url: releativePath + page.url,
-          children: []
+          children: [],
         };
-      }else{
+      } else {
         searchPage = {
           title: page.title,
           url: releativePath + page.url,
-          children: []
+          children: [],
         };
       }
 
-      newPages.push(searchPage)
+      newPages.push(searchPage);
 
-      if(page.children && Array.isArray(page.children)){
-        for(var j=0, len=page.children.length; j< len; j++){
+      if (page.children && Array.isArray(page.children)) {
+        for (var j = 0, len = page.children.length; j < len; j++) {
           var child = page.children[j];
-          if(num >= searchMaxNum ){
+          if (num >= searchMaxNum) {
             return newPages;
           }
-          if(child.content && child.content.toLowerCase().indexOf(searchText.toLowerCase()) !== -1){
+          if (
+            child.content &&
+            child.content.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+          ) {
             child.url = releativePath + child.url;
-            searchPage.children.push(child)
-            num++;          
+            searchPage.children.push(child);
+            num++;
           }
         }
       }
-    }    
+    }
     return newPages;
   }
-
-
 });
