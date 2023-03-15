b7db1b3bba36edc592f5409cd6de2bc134d37696
shuoshubao <759979885@qq.com>
2023-03-15 11:38:26 +0800

    docs: 更新链接

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,10 @@ banner:
   desc: '提高复用性, 更懂你的工具库'
   btns: 
     - { name: '开始', href: './documents/date.html', primary: true }
-    - { name: 'Github', href: 'https://github.com/shuoshubao/nbfe/tree/master/packages/tools', primary: true }
-  caption: '当前版本: v0.4.0'
+    - { name: 'Github', href: 'https://github.com/shuoshubao/tools', primary: true }
+  caption: '当前版本: v0.4.1'
 features: 
-  - { name: '零依赖', desc: '只依赖 lodash, 不搞一些基础功能的重复' }
+  - { name: '零依赖', desc: '只依赖 lodash, 不重复开发一些最基础的功能' }
   - { name: '懂业务', desc: '完全基于业务和组件库提炼, 没有花里胡哨的功能' }
   - { name: '优雅', desc: '每一个方法名都经过严格的思考, 让你用一次就记得住' }
 
@@ -16,10 +16,8 @@ footer:
     name: '硕鼠宝'
     href: 'https://github.com/shuoshubao/'
   links:
-    团队网址:
-      - { name: 'NBFE', href: 'https://github.com/shuoshubao/nbfe/' }
     Git仓库:
-      - { name: 'Github', href: 'https://github.com/shuoshubao/nbfe/tree/master/packages/tools' }
+      - { name: 'Github', href: 'https://github.com/shuoshubao/nbfe/tools' }
 
 ---
 
