6908d33b21df46b99fa64d2e6096751569b4ecfe
shuoshubao <759979885@qq.com>
2023-04-12 10:55:05 +0800

    ci: 移除无用的包

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import babel from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-terser'
 import pkg from './package.json'
 
@@ -61,6 +59,6 @@ export default [
       format: 'umd',
       banner: getBanner()
     },
-    plugins: [...plugins, nodeResolve(), commonjs(), terser()]
+    plugins: [...plugins, terser()]
   }
 ]
